61ad3ba564c4cbdb1e1a40f66f10c0fe93dd286c
Jonnathan Gonzalez <jonnathan@origenaudio.com>
2024-02-23 13:19:09 +0100

    API data retrive

--- MusicStore/views.py
+++ MusicStore/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+import requests
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        api_url = 'http://127.0.0.1:9000/api/simple/'
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            api_data = response.json()
+
+            formatted_data = [
+                {'title': item['title'], 'deezer_id': item['deezer_id']} for item in api_data]
+
+            context['api_data'] = formatted_data
+
+            print(context)
+        else:
+            context['error_message'] = 'Error obtaining data from the API'
+
+        return context
